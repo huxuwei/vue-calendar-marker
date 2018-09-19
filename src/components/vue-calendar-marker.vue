122298e3914a94adfa069778adf84ca65e55b0a5
huxuwei <hu_xuwei@163.com>
2018-09-19 08:40:26 +0800

    commit

--- src/components/vue-calendar-marker.vue
+++ src/components/vue-calendar-marker.vue
@@ -1,9 +1,12 @@
 <template>
-	<div>
-		<div>
-			<span><</span>
-			<span>></span>
+	<div class="wrapper">
+		<div class="cm-header">
+			<span style="left:5px">&lt;</span>
+			<span style="right:5px">&gt;</span>
 		</div>
+		<ul >
+			<li></li>
+		</ul>
 	</div>
 </template>
 
@@ -15,6 +18,12 @@ export default {
 			type: Array,
 			default(){
 				return []
+			}
+		},
+		initDate:{
+			type: Object | String,
+			default(){
+				return new Date()
 			}
 		}
 	},
@@ -31,5 +40,21 @@ export default {
 
 <style  scoped>
 
+.wrapper{
+	width: 100%;
+}
+.cm-header{
+	width: 500px;
+	height: 30px;
+	background: #1a52f1cf;
+	position: relative;
+	display: flex;
+	align-items: center;
+
+}
+.cm-header span{
+	position: absolute;
+	color: #ffffff;
+}
 </style>
 
